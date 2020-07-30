22c07eda747c6f054ddd8cb078f191798d56f633
Ariel Abreu <facekapow@outlook.com>
2020-07-30 14:29:58 -0400

    Darling build of adv_cmds-174.0.1

---
+++ gen/colldef/colldef_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char __colldefVersionString[];
+ extern const double __colldefVersionNumber;
+
+ const unsigned char __colldefVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:colldef  PROJECT:adv_cmds-" "\n";
+ const double __colldefVersionNumber __attribute__ ((used)) = (double)0.;

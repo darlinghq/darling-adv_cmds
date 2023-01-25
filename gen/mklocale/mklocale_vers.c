2cd9f1f0df5c47f5f0d5a59b235973a012a61abd
Thomas A <curioustommy@protonmail.com>
2023-01-24 17:38:49 -0800

    Update Source To adv_cmds-176.100.1

--- gen/mklocale/mklocale_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char __mklocaleVersionString[];
- extern const double __mklocaleVersionNumber;
-
- const unsigned char __mklocaleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:mklocale  PROJECT:adv_cmds-" "\n";
- const double __mklocaleVersionNumber __attribute__ ((used)) = (double)0.;
